e21ec62bbd94d030f740753d4936454d435a680c
scarlett <youjin9200@naver.com>
2018-08-17 13:06:57 +0900

    Update 3. 브루트포스 3-10_모든 순열_10974.java

--- 3. 브루트포스 3-10_모든 순열_10974.java
+++ 3. 브루트포스 3-10_모든 순열_10974.java
@@ -1,4 +1,55 @@
 /*
 문제링크 
 https://www.acmicpc.net/problem/10974
+해결 : 1부터 N 까지 배열에 차례대로 담아두고 next_permutation loop 돌리기 !
 */
+import java.util.Scanner;
+
+public class bf_3_10_모든순열_10974 {
+	public static boolean next_permutation(int[] A) {
+		// 예) 7 2 3 6 5 4 1
+		// 1. A[i-1] < A[i] 를 만족하는 i 찾기 : 6
+		int i = A.length - 1;
+		while (i > 0 && A[i-1] >= A[i]) {
+			i--;
+		}
+		if (i <= 0) {
+			return false;
+		}
+		// 2. j >= i 이면서 A[j] > A[i-1]를 만족하는 가장 큰 j 찾기  : 4
+		int j = A.length - 1;
+		while (A[j] <= A[i-1]) {
+			j--;
+		}
+		// 3. swap(A[i-1] <-> A[j]) : swap (3, 4)
+		int temp = A[i-1];
+		A[i-1] = A[j];
+		A[j] = temp;
+		// 4. A[i] 부터 뒤집기 : 6 5 3 1 -> 1 3 5 6
+		int k = A.length - 1;
+		while (i < k) {
+			temp = A[i];
+			A[i] = A[k];
+			A[k] = temp;
+			k--;
+			i++;
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] a = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = i+1;
+			System.out.print(a[i]+ " ");
+		}
+		System.out.println();
+		while (next_permutation(a)) {
+			for (int i = 0; i < N; i++) {
+				System.out.print(a[i]+ " ");
+			}
+			System.out.println();
+		}
+	}
+}
