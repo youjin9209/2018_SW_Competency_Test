eb48f1cb4b476a86d3c5b4bd52042c3318a9a251
scarlett <youjin9200@naver.com>
2018-08-22 23:49:59 +0900

    Update 3. 브루트포스 3-15_로또_6603.java

--- 3. 브루트포스 3-15_로또_6603.java
+++ 3. 브루트포스 3-15_로또_6603.java
@@ -2,84 +2,43 @@
 문제 링크
 https://www.acmicpc.net/problem/6603
 */
+/* 
+go (a, index, cnt)
+a : 입력으로 주어진 수 
+index : 선택할지 말지 결정해야 하는 인덱스 
+cnt : 현재까지 포함한 수의 개수 
+*/
 import java.util.*;
+
 public class bf_3_15_로또_6603 {
-    static boolean next_permutgetion(int[] a) {
-        int i = a.length-1;
-        while (i > 0 && a[i-1] >= a[i]) {
-            i -= 1;
-        }
-
-        if (i <= 0) {
-            return false;
-        }
-
-        int j = a.length-1;
-        while (a[j] <= a[i-1]) {
-            j -= 1;
-        }
-
-        int temp = a[i-1];
-        a[i-1] = a[j];
-        a[j] = temp;
-
-        j = a.length-1;
-        while (i < j) {
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-            i += 1;
-            j -= 1;
-        }
-        return true;
+    static ArrayList<Integer> lotto = new ArrayList<>();
+	public static void go (int[] a, int index, int cnt) {
+    	if (cnt == 6) {
+    		for (int num : lotto) {
+    			System.out.print(num + " ");
+    		}
+    		System.out.println();
+    		return;
+    	}
+    	int n = a.length;
+    	if (n == index)
+    		return;
+    	lotto.add(a[index]);
+    	go(a, index + 1, cnt + 1);
+    	lotto.remove(lotto.size() - 1);
+    	go(a, index + 1, cnt);
     }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         while (true) {
             int n = sc.nextInt();
-            if (n == 0) break;
+            if (n == 0) 
+            	break;
             int[] a = new int[n];
             for (int i=0; i<n; i++) {
                 a[i] = sc.nextInt();
             }
-            int[] d = new int[n];
-            for (int i=0; i<n; i++) {
-                if (i < n-6) d[i] = 0;
-                else d[i] = 1;
-            }
-            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-            do {
-                ArrayList<Integer> cur = new ArrayList<>();
-                for (int i=0; i<n; i++) {
-                    if (d[i] == 1) {
-                        cur.add(a[i]);
-                    }
-                }
-                ans.add(cur);
-            } while (next_permutgetion(d));
-            Collections.sort(ans, new Comparator<ArrayList<Integer>>() {
-                public int compare(ArrayList<Integer> l1, ArrayList<Integer> l2) {
-                    int n = l1.size();
-                    int m = l2.size();
-                    int i = 0;
-                    while (i < n && i < m) {
-                        int t1 = l1.get(i);
-                        int t2 = l2.get(i);
-                        if (t1 < t2) return -1;
-                        else if (t1 > t2) return 1;
-                        i += 1;
-                    }
-                    if (i == n && i != m) return -1;
-                    else if (i != n && i == m) return 1;
-                    return 0;
-                }
-            });
-            for (ArrayList<Integer> v : ans) {
-                for (int x : v) {
-                    System.out.print(x + " ");
-                }
-                System.out.println();
-            }
+            go (a, 0, 0);
             System.out.println();
         }
     }
