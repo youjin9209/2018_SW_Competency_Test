29c9ee1833e98ec7962bc3082e1be4a6a14e3610
scarlett <youjin9200@naver.com>
2018-08-27 15:41:00 +0900

    Update 1. DP 1-5_붕어빵판매하기_11052.java

--- 1. DP 1-5_붕어빵판매하기_11052.java
+++ 1. DP 1-5_붕어빵판매하기_11052.java
@@ -2,3 +2,25 @@
 문제링크 
 https://www.acmicpc.net/problem/11052
 */
+import java.util.Scanner;
+
+public class bf_1_5_붕어빵판매하기_11052 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] P = new int[1001];
+		for (int i = 1; i <= N; i++) {
+			P[i] = sc.nextInt();
+		}
+		int[] d = new int[1001];
+		for (int i = 1; i <= N; i++) {
+			d[i] = P[i];
+			for (int j = 1; j <= i; j++) {
+				if (d[i] < P[j] + d[i-j])
+					d[i] = P[j] + d[i-j];
+			}
+		}
+		System.out.println(d[N]);
+	}
+}
