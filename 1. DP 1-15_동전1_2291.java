fe2c3eb4e226b88f8a9fc2b5b42757a8f0f7eaf6
scarlett <youjin9200@naver.com>
2018-08-30 19:44:33 +0900

    Update 1. DP 1-15_동전1_2291.java

--- 1. DP 1-15_동전1_2291.java
+++ 1. DP 1-15_동전1_2291.java
@@ -2,3 +2,27 @@
 문제 링크 
 https://www.acmicpc.net/problem/2293
 */
+import java.util.Scanner;
+
+public class bf_1_15_동전1_2293 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int[] d = new int[k+1];
+        d[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                if (j-a[i] >= 0) {
+                    d[j] += d[j-a[i]];
+                }
+            }
+        }
+        System.out.println(d[k]);
+	}
+}
