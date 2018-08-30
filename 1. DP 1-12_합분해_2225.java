8e7a7dd3c7ca505f5127308ccd199b23ab393c42
scarlett <youjin9200@naver.com>
2018-08-30 16:36:40 +0900

    Update 1. DP 1-12_합분해_2225.java

--- 1. DP 1-12_합분해_2225.java
+++ 1. DP 1-12_합분해_2225.java
@@ -2,3 +2,23 @@
 문제 링크 
 https://www.acmicpc.net/problem/2225
 */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		long mod =  1000000000L;
+		int[][] d = new int[K+1][N+1];
+		d[0][0] = 1;
+		for (int k = 1; k <= K; k++) {
+			for (int i = 0; i <= N; i++) {
+				for (int l = 0; l <= i; l++) {
+					d[k][i] += d[k-1][i-l];
+					d[k][i] %= mod;
+				}
+			}
+		}
+		System.out.println(d[K][N]);
+	}
+}
