959e7cc4bfa337daced18ce0822c4f0896bd0aab
scarlett <youjin9200@naver.com>
2018-08-30 15:04:09 +0900

    Update 1. DP 1-9_가장긴증가하는부분수열_11053.java

--- 1. DP 1-9_가장긴증가하는부분수열_11053.java
+++ 1. DP 1-9_가장긴증가하는부분수열_11053.java
@@ -2,3 +2,31 @@
 문제 링크 
 https://www.acmicpc.net/problem/11053
 */
+import java.util.Scanner;
+
+public class bf_1_9_가장긴증가하는부분수열_11053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] a = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = sc.nextInt();
+		}
+		int[] d = new int[N];
+		d[0] = 1;
+		for (int i = 1; i < N; i++) {
+			d[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (a[i] > a[j] && d[i] < d[j] + 1)
+					d[i] = d[j] + 1;
+			}
+		}
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			if (max < d[i])
+				max = d[i];
+		}
+		System.out.println(max);
+	}
+}
