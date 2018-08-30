3b8a3e30241925321d3c057bbcc2c7fe3327cd8c
scarlett <youjin9200@naver.com>
2018-08-30 12:34:43 +0900

    Update 1. DP 1-6_쉬운계단수_10844.java

--- 1. DP 1-6_쉬운계단수_10844.java
+++ 1. DP 1-6_쉬운계단수_10844.java
@@ -1,4 +1,34 @@
 /*
 문제 링크 
 https://www.acmicpc.net/problem/10844
+변수가 되는걸 찾기 - N번째 
 */
+import java.util.Scanner;
+
+public class bf_1_6_쉬운계단수_10844 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		long mod = 1000000000L;
+		long[][] d = new long[N+1][10];
+		for (int i = 1; i <= 9; i++)
+			d[1][i] = 1;
+		
+		for (int i = 1; i <= N; i++) {
+			for(int k = 0; k <= 9; k++) {
+				if (k-1 >= 0)
+					d[i][k] += d[i-1][k-1];
+				if (k+1 <= 9)
+					d[i][k] += d[i-1][k+1];
+				d[i][k] %= mod;
+			}
+		}
+		long sum = 0;
+		for (int i = 0; i <= 9; i++) {
+			sum += d[N][i];
+			sum %= mod;
+		}
+		System.out.println(sum);
+	}
+}
