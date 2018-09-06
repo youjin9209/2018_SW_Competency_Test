47b1be219b3393fc2b2f47c128847d3031ea04d1
scarlett <youjin9200@naver.com>
2018-09-06 17:57:50 +0900

    Update 6. 모의sw역량테스트_1953_탈주범검거.java

--- 6. 모의sw역량테스트_1953_탈주범검거.java
+++ 6. 모의sw역량테스트_1953_탈주범검거.java
@@ -4,15 +4,22 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-
-public class 모의sw역량테스트_1953_탈주범검거 {
+class Pair {
+	int x, y;
+	Pair (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+class Solution {
 	static int N;
 	static int M;
 	static int L;
-	static int[][] map;
-	static int[][] dist;
+	static int[][] map = new int[50][50];
+	static int[][] dist = new int[50][50];
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
+	static Queue<Pair> queue = new LinkedList<Pair>();
 	public static boolean up (int x, int y) {
 		if (map[x][y] == 1 || map[x][y] == 2 || map[x][y] == 4 || map[x][y] == 7)
 			return true;
@@ -42,8 +49,8 @@ public class 모의sw역량테스트_1953_탈주범검거 {
 		for (int i = 0; i < N; i++) {
 			Arrays.fill(dist[i], 0);
 		}
-		Queue<Pair> queue = new LinkedList<Pair>();
-		queue.add(new Pair(startX, startY));
+		Pair s = new Pair(startX, startY);
+		queue.add(s);
 		dist[startX][startY] = 1;
 		
 		while (!queue.isEmpty()) {
@@ -52,13 +59,14 @@ public class 모의sw역량테스트_1953_탈주범검거 {
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
+				Pair n = new Pair(nx, ny);
 				if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
 					if (map[nx][ny] == 0) continue;
 					if (dist[nx][ny] != 0) continue;
 					if (up(x, y)) { // 위방향이 뚫려 있으면 위에만 보면 된다 
 						if (i == 0) { // 다음이 위 칸일 때 
 							if (down(nx, ny)) { // 다음칸이 아래가 뚫려 있어야 함 
-								queue.add(new Pair(nx, ny));
+								queue.add(n);
 								dist[nx][ny] = dist[x][y] + 1;
 							}
 						}
@@ -66,7 +74,7 @@ public class 모의sw역량테스트_1953_탈주범검거 {
 					if (down(x, y)) {
 						if (i == 1) {
 							if (up(nx, ny)) {
-								queue.add(new Pair(nx, ny));
+								queue.add(n);
 								dist[nx][ny] = dist[x][y] + 1;
 							}
 						}
@@ -74,7 +82,7 @@ public class 모의sw역량테스트_1953_탈주범검거 {
 					if (right(x, y)) {
 						if (i == 2) {
 							if (left(nx, ny)) {
-								queue.add(new Pair(nx, ny));
+								queue.add(n);
 								dist[nx][ny] = dist[x][y] + 1;
 							}
 						}
@@ -82,12 +90,13 @@ public class 모의sw역량테스트_1953_탈주범검거 {
 					if (left(x, y)) {
 						if (i == 3) {
 							if (right(nx, ny)) {
-								queue.add(new Pair(nx, ny));
+								queue.add(n);
 								dist[nx][ny] = dist[x][y] + 1;
 							}
 						}
 					}
 				}
+				n = null;
 			}
 		}
 		for (int p = 1; p <= L; p++) {
@@ -98,6 +107,7 @@ public class 모의sw역량테스트_1953_탈주범검거 {
 				}
 			}
 		}
+		s = null;
 		return result;
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -110,14 +120,12 @@ public class 모의sw역량테스트_1953_탈주범검거 {
 			int x = Integer.parseInt(token[2]); // 맨홀 x 좌표 
 			int y = Integer.parseInt(token[3]); // 맨홀 y 좌표 
 			L = Integer.parseInt(token[4]); // 소요 시간 
-			map = new int[N][M];
 			for (int i = 0; i < N; i++) {
 				String[] line = bf.readLine().split(" ");
 				for (int j = 0; j < M; j++) {
 					map[i][j] = Integer.parseInt(line[j]);
 				}
 			}
-			dist = new int[N][M];
 			int result = 0;
 			result = bfs(x, y);
 			System.out.println("#"+t+" "+result);
