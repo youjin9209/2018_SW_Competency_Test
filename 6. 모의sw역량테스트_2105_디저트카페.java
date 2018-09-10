2b3e7215844c5ef2c9b257acab080fcc1ff4895c
scarlett <youjin9200@naver.com>
2018-09-10 16:39:49 +0900

    Update 6. 모의sw역량테스트_2105_디저트카페.java

--- 6. 모의sw역량테스트_2105_디저트카페.java
+++ 6. 모의sw역량테스트_2105_디저트카페.java
@@ -20,34 +20,33 @@ public class Solution_2105_디저트카페 {
 				return;
 		}
 		if(check[x][y]) return;
-		
-		// 2. 처음지점 다시 돌아왔을 때 - 최대값 갱신 
+		// 2. 종결조건 : 처음지점 다시 돌아왔을 때 - 최대값 갱신 
 		if (dir == 3 && startX == x && startY == y) {
 			if (max < sum)
 				max = sum;
 			return;
 		}
-		// 3. check[][], dessert, sum 값 
+		// 3. check[][], dessert, sum 값 대입 
 		if (x != startX && y != startY)
 			check[x][y] = true;
 		dessert[map[x][y]] = true;
 		sum += 1;
-		// 4. 같은방향으로 go straight (얘가 먼저간다 )
+		// 4. 같은방향 (dir) 으로 go
 		int nx, ny;
 		if (dir < 4) {
 			nx = x + dx[dir];
 			ny = y + dy[dir];
 			dfs(nx, ny, dir, sum);
 		}
-		// 4. 다른방향으로 go 
+		// 4. 다른방향 (dir + 1)으로 go
 		if (dir + 1 < 4) {
 			nx = x + dx[dir+1];
 			ny = y + dy[dir+1];
 			dfs(nx, ny, dir+1, sum);
 		}
-		// 5. backtracking 해야하니까 false 시켜 
-		dessert[map[x][y]] = false;
+		// 5. 다른방향으로도 안될때 - backtracking 해야하니까 check[][], dessert, sum false 시켜 
 		check[x][y] = false;
+		dessert[map[x][y]] = false;
 		sum -= 1;
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -69,6 +68,8 @@ public class Solution_2105_디저트카페 {
 				for (int j = 0; j < N; j++) {
 					startX = i;
 					startY = j;
+					if (i == 0 && j == 1) 
+						System.out.println();
 					dfs(i, j, 0, 0);
 				}
 			}
