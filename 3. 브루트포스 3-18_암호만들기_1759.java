d88e9fef1eedbde45842c7826bc94209e3adff2c
scarlett <youjin9200@naver.com>
2018-09-14 14:00:46 +0900

    Update 3. 브루트포스 3-18_암호만들기_1759.java

--- 3. 브루트포스 3-18_암호만들기_1759.java
+++ 3. 브루트포스 3-18_암호만들기_1759.java
@@ -3,50 +3,53 @@ import java.util.Scanner;
 /*
  1) 최소 한 개의 모음과 최소 두 개의 자음
  2) 증가수열 
- 3) go (n, alpha, password, i)
-n: 암호 길이 
-alpha : 사용가능한 알파벳 
+ 3) go (password, i)
 password : 현재까지 만든 암호 
 i : 사용할지 말지 결정해야하는 인덱스 
  */
-public class bf_3_18_암호만들기_1759 {
-	public static boolean check (String password) {
+public class Main {
+	static int L;
+	static int C;
+	static char[] a;
+	public static boolean check(String str) {
 		int ja = 0;
 		int mo = 0;
-		for (char x : password.toCharArray()) {
-			if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')
-				mo += 1;
-			else
-				ja += 1;
+		int len = str.length();
+		for (int i = 0; i < len; i++) {
+			char word = str.charAt(i);
+			if (word == 'a' || word == 'e' || word == 'i' || word == 'o' || word == 'u') {
+				mo++;
+			} else {
+				ja++;
+			}
 		}
 		return ja >= 2 && mo >= 1;
 	}
-	public static void go(int n, char[] alpha, String password, int i) {
-		if (password.length() == n) {
-			if (check(password)) {
-				System.out.println(password);
+	public static void go(int index, String str) {
+		if (str.length() == L) {
+			if (check(str)) {
+				System.out.println(str);
 			}
 			return;
 		}
-		if (i >= alpha.length) 
-			return;
-		// i 번째 알파벳을 사용 할 때 
-		go(n, alpha, password + alpha[i], i+1);
-		// 안사용 할 때 
-		go(n, alpha, password, i+1);
-		
+		if (index >= C) return;
+		// index 상태에서 
+		// 1) index + 1 의 단어를 추가 한다 
+		go(index + 1, str+a[index]);
+		// 2) index + 1 의 단어를 추가 안 한다 
+		go(index + 1, str);
 	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int L = sc.nextInt();
-		int C = sc.nextInt();
-		char[] alpha = new char[C];
-		sc.nextLine();
-		String[] line = sc.nextLine().split(" ");
-		for (int i = 0; i < C; i++) {
-			alpha[i] = line[i].charAt(0);
-		}
-		Arrays.sort(alpha); // 증가수열이니까 일단 정렬하고 시작
-		go(L, alpha, "", 0);
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner sc = new Scanner(System.in);
+        L = sc.nextInt();
+        C = sc.nextInt();
+        sc.nextLine();
+        a = new char[C];
+        String[] line = sc.nextLine().split(" ");
+        for (int i = 0; i < C; i++) {
+        	a[i] = line[i].charAt(0);
+        }
+        Arrays.sort(a);
+        go(0, "");
+    }
 }
