c5dcc399811198ff9a6eba794bfadda9ed970734
scarlett <youjin9200@naver.com>
2018-10-02 15:06:44 +0900

    Update 6. 모의sw역량테스트_1949_등산로조성.java

--- 6. 모의sw역량테스트_1949_등산로조성.java
+++ 6. 모의sw역량테스트_1949_등산로조성.java
@@ -1,39 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-class Pair {
+import java.util.Scanner;
+class Point {
 	int x, y;
-	Pair (int x, int y) {
+	Point (int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 }
-class Solution {
-	static int[][] map = new int[8][8];
+public class Solution {
 	static int N;
 	static int K;
+	static int[][] map;
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
-	static int[][] dist = new int[8][8];
-	static int[][] d = new int[8][8];
-	static Queue<Pair> queue = new LinkedList<Pair>();
-	public static int bfs (int startX, int startY) {
-		queue.add(new Pair(startX, startY));
-		dist[startX][startY] = 1;
+	public static int bfs1(int i, int j) {
 		int result = 0;
+		int[][] dist = new int[N][N];
+		Queue<Point> queue = new LinkedList<Point>();
+		queue.add(new Point(i, j));
+		dist[i][j] = 1;
 		while (!queue.isEmpty()) {
-			Pair p = queue.remove();
-			int x = p.x;
-			int y = p.y;
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
+			Point p = queue.remove();
+			int x = p.x; int y = p.y;
+			for (int idx = 0; idx < 4; idx++) {
+				int nx = x + dx[idx];
+				int ny = y + dy[idx];
 				if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
 					if (map[nx][ny] >= map[x][y]) continue;
-					queue.add(new Pair(nx, ny));
+					queue.add(new Point(nx, ny));
 					dist[nx][ny] = dist[x][y] + 1;
 					if (result < dist[nx][ny])
 						result = dist[nx][ny];
@@ -42,63 +37,64 @@ class Solution {
 		}
 		return result;
 	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(bf.readLine());
-		for (int t = 1; t <= T; t++) {
-			String[] line = bf.readLine().split(" ");
-			N = Integer.parseInt(line[0]);
-			K = Integer.parseInt(line[1]);
-			int max = Integer.MIN_VALUE;
-			for (int i = 0; i < N; i++) {
-				String[] token = bf.readLine().split(" ");
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(token[j]);
-					if (max <= map[i][j]) {
-						max = map[i][j];
+	public static int bfs2(int i, int j, int maxVal) {
+		int result = Integer.MIN_VALUE;
+		int[][] dist = new int[N][N];
+		Queue<Point> queue = new LinkedList<Point>();
+		queue.add(new Point(i, j));
+		dist[i][j] = 0;
+		while (!queue.isEmpty()) {
+			Point p = queue.remove();
+			int x = p.x; int y = p.y;
+			for (int idx = 0; idx < 4; idx++) {
+				int nx = x + dx[idx];
+				int ny = y + dy[idx];
+				if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+					if (map[nx][ny] <= map[x][y]) continue;
+					queue.add(new Point(nx, ny));
+					dist[nx][ny] = dist[x][y] + 1;
+					if (map[nx][ny] == maxVal) {
+						if (result < dist[nx][ny])
+							result = dist[nx][ny];
 					}
+				}
+			}
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			N = sc.nextInt();
+			K = sc.nextInt();
+			int maxVal = Integer.MIN_VALUE;
+			map = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					map[i][j] = sc.nextInt();
+					if (maxVal < map[i][j])
+						maxVal = map[i][j];
 				}
 			}
 			int result = 0;
-			int cnt = 0;
-			for (int i = 0; i < N; i++) { // 다깎아 놓고 시작 
+			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					for (int h = 0; h <= K; h++) {
-						map[i][j] -= h; // 깎고 
-						cnt = bfs(i, j);
-						// (bfs돌린 x,y : cnt) + (최고 높이 봉우리 까지 : maxAdd)
-						for (int q = 0; q < N; q++) {
-							Arrays.fill(d[q], 0);
-						}
-						queue.add(new Pair(i, j));
-						int maxAdd = Integer.MIN_VALUE;
-						while (!queue.isEmpty()) {
-							Pair p = queue.remove();
-							for (int idx = 0; idx < 4; idx++) {
-								int nx = p.x + dx[idx];
-								int ny = p.y + dy[idx];
-								if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-									if (map[nx][ny] > map[p.x][p.y]) {
-										d[nx][ny] = d[p.x][p.y] + 1;
-										queue.add(new Pair(nx, ny));
-										if (map[nx][ny] == max) {
-											if (maxAdd < d[nx][ny])
-												maxAdd = d[nx][ny];
-										}
-									}
-								}
-							}
-						}
-						if (cnt == 0)
-							cnt++;
-						cnt += maxAdd;
-						if (result < cnt)
-							result = cnt;
-						map[i][j] += h; //깎은거 초기화 
+					for (int k = 0; k <= K; k++) {
+						map[i][j] -= k; // 깎고 
+						// 1) 깍은 지점에서 시작하여 최소 높이지점 까지 탐색 - bfs1
+						int len1 = bfs1(i, j);
+						if (len1 == 0)  // 깎은 지점이 높이가 최소지점일 때 (탐색 못했을 때 )
+							len1++;
+						// 2) 깍은 지점에서 시작해 최대 높이 지점까지 탐색 (maxVal까지) - bfs2
+						int len2 = bfs2(i, j, maxVal);
+						if (result < len1 + len2)
+							result = len1 + len2;
+						map[i][j] += k;// 복구 
 					}
 				}
 			}
-			System.out.println("#"+t+" "+result);
+			System.out.println("#"+test_case+" "+result);
 		}
 	}
 }
