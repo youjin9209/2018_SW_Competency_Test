e0b91d26595f63b8c39f609d2f25968928a62887
scarlett <youjin9200@naver.com>
2018-10-04 11:07:39 +0900

    Update 6. 모의sw역량테스트_2105_디저트카페.java

--- 6. 모의sw역량테스트_2105_디저트카페.java
+++ 6. 모의sw역량테스트_2105_디저트카페.java
@@ -1,79 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Solution_2105_디저트카페 {
+import java.util.Scanner;
+class Point {
+	int x, y;
+	Point (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+public class Solution {
 	static int N;
 	static int[][] map;
+	static boolean[] biteCheck; // 해당 디저트 종류를 먹었는 지 check 
+	static boolean[][] check; // 해당 좌표를 건넜는지 check 
+	static int[][] dist; // 먹은 디저트 개수 저장 
 	static int[] dx = {1, 1, -1, -1};
 	static int[] dy = {1, -1, -1, 1};
-	static boolean[][] check;
-	static boolean[] dessert;
-	static int max;
+	static int dessertSum;
 	static int startX;
 	static int startY;
-	public static void dfs(int x, int y, int dir, int sum) {
-		// 1. 제한조건 check 
-		if (x < 0 || x >= N || y < 0 || y >= N) return;
-		if (dessert[map[x][y]]) {
-			if (!(x == startX && y == startY))
-				return;
-		}
-		if(check[x][y]) return;
-		// 2. 종결조건 : 처음지점 다시 돌아왔을 때 - 최대값 갱신 
-		if (dir == 3 && startX == x && startY == y) {
-			if (max < sum)
-				max = sum;
+	public static void dfs(int x, int y, int idx, int sum) {
+		// 1) 종결 조건 : 다시 시작포인트로 돌아왔을 때 
+		if (idx == 3 && startX == x && startY == y) {
+			if (dessertSum < sum)
+				dessertSum = sum;
 			return;
 		}
-		// 3. check[][], dessert, sum 값 대입 
-		if (x != startX && y != startY)
+		// 2) return 조건 
+		if (x < 0 || x >= N || y < 0 || y >= N) return;
+		if (biteCheck[map[x][y]]) return;
+		if (check[x][y]) return;
+		if (idx > 3) return;
+		// 3) go 
+		biteCheck[map[x][y]] = true;
+		if (!(startX == x && startY == y))
 			check[x][y] = true;
-		dessert[map[x][y]] = true;
-		sum += 1;
-		// 4. 같은방향 (dir) 으로 go
-		int nx, ny;
-		if (dir < 4) {
-			nx = x + dx[dir];
-			ny = y + dy[dir];
-			dfs(nx, ny, dir, sum);
-		}
-		// 4. 다른방향 (dir + 1)으로 go
-		if (dir + 1 < 4) {
-			nx = x + dx[dir+1];
-			ny = y + dy[dir+1];
-			dfs(nx, ny, dir+1, sum);
-		}
-		// 5. 다른방향으로도 안될때 - backtracking 해야하니까 check[][], dessert, sum false 시켜 
+		int nx = x + dx[idx];
+		int ny = y + dy[idx];
+		dfs(nx, ny, idx, sum + 1); // 가던 방향으로 쭉 간다 -> 이게 최대가 될 확률이 높으니까 먼저 탐색 
+		dfs(nx, ny, idx + 1, sum + 1); // 가던 방향으로 안되면 방향 튼다 
+		// 4) return 했을 경우 부모노드 초기화 
+		biteCheck[map[x][y]] = false;
 		check[x][y] = false;
-		dessert[map[x][y]] = false;
-		sum -= 1;
 	}
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(bf.readLine());
-		for (int t = 1; t <= T; t++) {
-			max = -1;
-			N = Integer.parseInt(bf.readLine());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			// 1. initialize 
+			N = sc.nextInt();
 			map = new int[N][N];
 			for (int i = 0; i < N; i++) {
-				String[] token = bf.readLine().split(" ");
 				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(token[j]);
+					map[i][j] = sc.nextInt();
 				}
 			}
-			check = new boolean[N][N];
-			dessert = new boolean[101];
+			// 2. process 
+			dist = new int[N][N];
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					startX = i;
-					startY = j;
-					if (i == 0 && j == 1) 
-						System.out.println();
+					check = new boolean[N][N];	
+					biteCheck = new boolean[101];
+					startX = i; startY = j;
 					dfs(i, j, 0, 0);
+					dist[i][j] = dessertSum;
+					dessertSum = 0;
 				}
 			}
-			System.out.println("#"+t+" "+max);
+			int result = 0;
+			// 3. get summation 
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (result < dist[i][j])
+						result = dist[i][j];
+				}
+			}
+			if (result == 0)
+				result = -1;
+			System.out.println("#"+test_case+" "+result);
 		}
 	}
 }
