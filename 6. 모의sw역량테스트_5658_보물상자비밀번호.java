5eec956773409f27efdbf32b6e306b86533be184
scarlett <youjin9200@naver.com>
2018-10-18 23:08:08 +0900

    Update 6. 모의sw역량테스트_5658_보물상자비밀번호.java

--- 6. 모의sw역량테스트_5658_보물상자비밀번호.java
+++ 6. 모의sw역량테스트_5658_보물상자비밀번호.java
@@ -1,54 +1,53 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Solution {
-	static int N;
-	static int K;
-	static char[] a;
-	static ArrayList<String> al;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+		int T;
+		T = sc.nextInt();
 		for (int test_case = 1; test_case <= T; test_case++) {
-			// 1. initialize 
-			N = sc.nextInt();
-			K = sc.nextInt();
-			a = new char[N];
+			int N = sc.nextInt(); // 숫자 개수 
+			int K = sc.nextInt(); // 크기 순서 
 			sc.nextLine();
-			a = sc.nextLine().toCharArray();
-			al = new ArrayList<String>();
-			for (int turn = 0; turn < N/4; turn++) {
-				// 2. 중복 체크하며 넣기 
+			String[] arr = sc.nextLine().split("");
+			ArrayList<String> resultList = new ArrayList<String>();
+			for (int p = 0; p < N/4; p ++) { // 언제 한바퀴 다 도는지 확인 !!
+				// 1) 중복 검사하고 삽입 
 				for (int i = 0; i < N; i += N/4) {
 					String line = "";
-					for (int j = 0; j < N/4 ; j++) {
-						line += a[i+j];
+					for (int j = 0; j < N/4; j++) {
+						line += arr[i+j];
 					}
-					boolean check = false;
-					int len = al.size();
-					for (int p = 0;  p < len; p++) {
-						if (al.get(p).equals(line)) {
-							check = true;
+					boolean flag = false;
+					int len = resultList.size();
+					for (int k = 0; k < len; k++) {
+						if (resultList.get(k).equals(line)) {
+							flag = true;
 							break;
 						}
 					}
-					if (!check)
-						al.add(line);
+					if (!flag) resultList.add(line);
 				}
-				// 3. 시계방향으로 돌리기 - 배열 주소복사는 위험하니까 그냥 값 넣어줘 
-				char[] temp = new char[N];
-				for (int i = 0; i < N; i++) {
-					temp[i] = a[(i+1)%N];
+				// 2) 시계 방향으로 회전 
+				String[] temp = new String[N];
+				for (int idx = 0; idx < N; idx++) {
+					temp[(idx+1)%N] = arr[idx];
 				}
-				for (int i = 0; i < N; i++) {
-					a[i] = temp[i];
+				for (int a = 0; a < N; a++) {
+					arr[a] = temp[a];
 				}
 			}
-			Collections.sort(al);
-			int idx = al.size() - K;
-			String result = al.get(idx);
-			System.out.println("#"+test_case+" "+Integer.parseInt(result, 16));
+			Collections.sort(resultList);
+			int len = resultList.size() - K;
+			String ans = resultList.get(len);
+			int result = Integer.parseInt(ans, 16);
+			System.out.println("#"+test_case+" "+result);
 		}
 	}
 }
